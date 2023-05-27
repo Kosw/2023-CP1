e44112f3e4cf180827331dfbdc793597d4aca557
kosw <swko4512@gmail.com>
2023-05-27 15:49:22 +0900

    com

--- telephonearray1.java
+++ telephonearray1.java
@@ -15,6 +15,7 @@ class Phone {//클래스 생성
 }
 
 class Telephone extends Phone {//클래스 생성
+
     public Telephone(String owner) {//메소드 생성
         super(owner);//상속자의 인스턴스 변수 가져오기
     }
@@ -31,23 +32,33 @@ class Smartphone extends Telephone {//클래스 생성
         super(owner);//상속자의 인스턴스 변수 가져오기
         this.game = game;//입력받은 값 인스턴스 변수에 넣기
     }
-
     public String playGame() {//메소드 생성
         return owner + "은(는) " + game + " 게임을 하는 중이다.";//반환
     }
 }
 
-public class phone1 {
+public class Main {
     public static void main(String[] args) {
-        Phone p = new Phone("홍길동");//객체 생성
-        Telephone t = new Telephone("장길산");//객체 생성
-        Smartphone s = new Smartphone("Peter", "테트리스");//객체 생성
-    
-        System.out.println(p.talk());//리턴한 값 출력
-        System.out.println(t.autoAnswering());//리턴한 값 출력
-        System.out.println(s.playGame());//리턴한 값 출력
+        Phone[] arr = {//객체 배열을 만들고 배열에 객체를 넣는다.
+                new Phone("홍길동"),
+                new Telephone("장길산"),
+                new Smartphone("Peter", "테트리스")
+        };
+
+        for (Phone phone : arr) {//배열 크기만큼 반복
+            switch (phone.getClass().getSimpleName()) {//스위치문으로 판별
+                case "Smartphone"://만약 스마트폰이면 아래를 실행하고 브레이크
+                    System.out.println(((Smartphone) phone).playGame());
+                    break;
+                case "Telephone"://만약 텔레폰이면 아래를 실행하고 브레이크
+                    System.out.println(((Telephone) phone).autoAnswering());
+                    break;
+                default://해당되는게 없으면 아래를 실행하고 브레이크
+                    System.out.println(phone.talk());
+                    break;
+            }
+        }
     }
 }
-
 
  */
